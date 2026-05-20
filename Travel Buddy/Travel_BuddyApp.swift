c0e490ff568a92c3c29d2672d2f9b6a9f4e78937
Juan <juankurniawan04@gmail.com>
2026-05-20 13:40:22 +0800

    Project Init

---
+++ Travel Buddy/Travel_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_BuddyApp.swift
+//  Travel Buddy
+//
+//  Created by Agustinus Juan Kurniawan on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Travel_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
