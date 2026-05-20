58c4cd607a14b2d2991e85e7805b24f14a4b1967
Juan <juankurniawan04@gmail.com>
2026-05-20 13:40:22 +0800

    Project Init

---
+++ Travel Buddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel Buddy
+//
+//  Created by Agustinus Juan Kurniawan on 20/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
